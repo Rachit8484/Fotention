1be322406bc64cb26b4dc2fd5f04a52bbd187691
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2019-01-27 09:44:19 -0600

    1.1

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react"
+
+// Main navbar
+export default class NavBar extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+}
+
+// The Gradient Title Logo
+class TitleLogo extends Component {
+
+}
+
+// Specifically Properties of the User text and icon
+class User extends Component {
+
+}
+
+// User point value that dynamically updates based on server
+class Points extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            pointVal = 0,
+        };
+    }
+
+
+}
